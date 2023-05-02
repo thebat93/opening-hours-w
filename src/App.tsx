0a8d7d0bf0a372724df2c1a3914647629c219caa
Igor Alekseev <ais_93@mail.ru>
2023-05-02 18:18:39 +0300

    Main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { OpeningHoursCard } from "./modules/OpeningHours/view";
+import { RawData } from "./modules/OpeningHours/types";
+import data from './data.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <OpeningHoursCard data={data as RawData} />
+  </div>
+);
 
 export default App;
